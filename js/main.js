3dd4b25336f3b7e13a1dd0e280dc6b43cdcd8d96
sahil-vanarse <sahilvanarse13@gmail.com>
2024-06-10 22:11:22 +0530

    Done Everything now just need to connect to the flask file

--- js/main.js
+++ js/main.js
@@ -12,26 +12,49 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    let top = window.scrollY;
     sections.forEach(sec => {
-        let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-        }      
+        }
     });
 
     // sticky navLinks
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    // remove toggle icon and navbar
+    // remove toggle icon and navbar when scrolling
+    
     menuIcon.classList.remove('fa-xmark');
     navbar.classList.remove('active');
-
+    
 };
+
+// scroll reveal 
+ScrollReveal({
+    distance : '80px',
+    duration : 2000,
+    delay : 200,
+});
+
+ScrollReveal().reveal('.home-content, header', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+
+
+// typed js
+const typed = new Typed('.multiple-text', {
+    strings: ['Python Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true // This ensures the typing animation loops
+});
